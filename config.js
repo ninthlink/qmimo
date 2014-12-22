153d13d8285ddf47004736f3ae5b5938c17ec9ed
alex chousmith <alex@ninthlink.com>
2014-12-22 10:00:58 -0800

    QMIMO-7 reorder config.js and make comments more straightforward & compact?

--- config.js
+++ config.js
@@ -1,47 +1,39 @@
 /**
- * qmimo.config.js 
+ * config.js
+ *
+ * Handles setting up some configurable variables for the GUI
+ * including # of connected devices, time before refreshing pieces,
+ * and whether or not to generate fake Throughputs data.
  */
-/**
- * Specify the # of devices we should expect tput from
- */
+// The # of devices we can expect tput from
 var QMIMO_NUMBER_OF_DEVICES = 6;
-/**
- * Set the # of ms to wait to refresh tput data
- */
+
+// # of ms to wait to refresh device tput data
 var QMIMO_REFRESH_TPUT_MS = 2000;
-/**
- * Set the # of ms delay when switching MU / SU modes
- */
+
+// # of ms downtime delay when switching MU / SU modes
 var QMIMO_SWITCH_DELAY_MS = 3000;
-/**
- * Specify which directory (inside the overall qmimo GUI folder)
- * we should expect to find our tput .txt files in
- */
+
+// Directory (inside the overall qmimo GUI folder?) to find tput files
 var QMIMO_TPUT_DATA_DIR = 'fake-demo-contents';
-/**
- * How many decimals we should show for our MU Gain number
- */
-var QMIMO_MU_GAIN_DECIMAL_PLACES = 2;
-/**
- * Optionally, we can control the actual file name format for our tput .txt
- * 
- * the # gets replaced by the actual numbers, so "tput#.txt" = "tput1.txt"...
- * ranging from 1 to QMIMO_NUMBER_OF_DEVICES above
- */
-var QMIMO_TPUT_FILE_NAME_FORMAT = 'tput#.txt';
-/**
- * Define which mode the demo should start in
- * 'mu' for Multi-User, 'su' for Single-User
- */
+
+// Which Mode to start in : 'mu' = Multi User, 'su' = Single User
 var QMIMO_INITIAL_MODE = 'mu';
+
+// How many decimals to show for the "MU Gain" number, in case we want to tweak
+var QMIMO_MU_GAIN_DECIMAL_PLACES = 2;
+
+// Optional control actual file name format for tput .txt : '#' is replaced
+var QMIMO_TPUT_FILE_NAME_FORMAT = 'tput#.txt';
+
 /**
+ * Fake Demo Throughput Numbers Generation
+ *
  * Specify whether we'll have actual data in coming in or if we should fake it.
  * If true, then use our fake-demo-contents/qgen.php script
  * to generate demo throughput tput data at the intervals specified below...
  */
 var QMIMO_FAKE_DEMO = true;
-/**
- * How many ms to wait before re-looping through above..
- * should probably be <= QMIMO_REFRESH_TPUT_MS
- */
+
+// Time to wait before re-generating demo #s. probably <= QMIMO_REFRESH_TPUT_MS
 var QMIMO_FAKE_DEMO_LOOP_MS = 1000;
