9c0445f0751888cd4f157b3aa86aa27b63ec4358
alex chousmith <alex@ninthlink.com>
2014-12-22 11:06:25 -0800

    QMIMO-7 clean up some guiCtrl comments & variables, & move the "MU Gain" decimal rounding out of code and in to the partials/gui.html since we can apparently just use https://docs.angularjs.org/api/ng/filter/number

--- app/controllers/gui.js
+++ app/controllers/gui.js
@@ -10,8 +10,7 @@ angular
   // app state setup for this Controller
   .config([
     '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    function($stateProvider) {
       $stateProvider
         .state('gui', {
           url: '/',
@@ -27,39 +26,34 @@ angular
   ])
   .controller('guiCtrl', guiCtrl);
 // inject Controller dependencies
-guiCtrl.$inject = [ '$scope', '$rootScope', '$state', '$stateParams', '$window', 'tputFactory', 'fakeTputGeneratorFactory', 'initialData', '$timeout' ];
+guiCtrl.$inject = [ '$scope', '$rootScope', '$timeout', 'tputFactory', 'fakeTputGeneratorFactory', 'initialData' ];
 /**
  * our main Controller
  */
-function guiCtrl( $scope, $rootScope, $state, $stateParams, $window, tputFactory, fakeTputGeneratorFactory, initialData, $timeout ) {
-  // some initial GUI setup
-  $scope.switchleft = true;
-  $rootScope.loading = false;
-  $scope.switchdelay = QMIMO_SWITCH_DELAY_MS;
+function guiCtrl( $scope, $rootScope, $timeout, tputFactory, fakeTputGeneratorFactory, initialData ) {
   // populate initialData from our tputFactory
   console.log(': initialData :');
   console.log(initialData);
+  // some initial GUI setup
+  $rootScope.loading = false;
   $rootScope.mode = initialData.mode;
+  $scope.switchleft = ( $rootScope.mode === 'mu' );
   $scope.devices = initialData.tputs;
   $scope.devicenum = $scope.devices.length;
+  $scope.roundtotals = QMIMO_TPUT_TOTALS_DECIMAL_PLACES;
+  $scope.roundgain = QMIMO_MU_GAIN_DECIMAL_PLACES;
   // recalculate our top MU & SU numbers based on all devices' tputs
   $scope.retotal = function() {
-    var new_mu = 0,
-        new_su = 0;
+    var mu_total = 0,
+        su_total = 0;
     angular.forEach( $scope.devices, function( d, k ) {
-      new_mu += d[1];
-      new_su += d[2];
+      mu_total += d[1];
+      su_total += d[2];
     });
-    $scope.mu_total = new_mu;
-    $scope.su_total = new_su;
-    // round gain to set # of decimals?
-    var d = Math.pow( 10, QMIMO_MU_GAIN_DECIMAL_PLACES );
-    // also make sure we don't accidentally try to device by 0
-    if ( new_su > 0 ) {
-      $scope.mu_gain = Math.round( new_mu * d / new_su ) / d;
-    } else {
-      $scope.mu_gain = 0;
-    }
+    $scope.mu_total = mu_total;
+    $scope.su_total = su_total;
+    // also make sure we don't accidentally try to divide by 0
+    $scope.mu_gain = ( su_total > 0 ) ? mu_total / su_total : 0;
   };
   $scope.retotal();
   // set timeout to re pull tput datas?
@@ -79,14 +73,12 @@ function guiCtrl( $scope, $rootScope, $state, $stateParams, $window, tputFactory
     }, QMIMO_REFRESH_TPUT_MS );
   };
   $scope.reloadTput();
-  // similar call for fake generating numbers via our fakeTputGeneratorFactory
+  // similar idea to fake generating numbers via fakeTputGeneratorFactory
   var tputGenTimer;
   $scope.kickGenerator = function() {
     $timeout.cancel( tputGenTimer );
     tputGenTimer = $timeout( function() {
       fakeTputGeneratorFactory.genDeviceTput().then(function(results) {
-        //console.log('finished re-generating demo numbers?');
-        //console.log(results);
         $scope.kickGenerator();
       });
     }, QMIMO_FAKE_DEMO_LOOP_MS );
@@ -98,25 +90,27 @@ function guiCtrl( $scope, $rootScope, $state, $stateParams, $window, tputFactory
   // action(s) to take when a button is pressed to switch between MU/SU mode
   $scope.switchMode = function() {
     if ( $rootScope.loading === false ) {
+      var prev = $rootScope.mode;
+      var wait = QMIMO_SWITCH_DELAY_MS;
+      var newmode = ( prev === 'mu' ? 'su' : 'mu' );
+      console.log( 'switching modes after '+ wait +'ms : from ' + prev );
+      // $scope.switchleft boolean controls actual position of UI switch
       $scope.switchleft = !$scope.switchleft;
-      var prevmode = $rootScope.mode;
-      var newmode = ( prevmode === 'mu' ? 'su' : 'mu' );
-      console.log( 'switching modes : from ' + prevmode );
-      console.log( '( after delay of '+ $scope.switchdelay +'ms )' );
+      // show "loading" mode rather than 'mu' or 'su'
       $rootScope.loading = true;
       $rootScope.mode = '';
       // pause tput # getting..
       $timeout.cancel( tputTimer );
-      // after our QMIMO_SWITCH_DELAY_MS, turn the GUI back on
+      // after QMIMO_SWITCH_DELAY_MS, poll new tput data & turn GUI back on?
       $timeout(function() {
         $scope.$apply( function() {
           $rootScope.mode = newmode;
           $rootScope.loading = false;
           // and after the delay, start getting the data again?
           $scope.reloadTput();
-          console.log( 'switched from '+ prevmode +' to '+ $scope.mode );
+          console.log( 'switched from '+ prev +' to '+ $scope.mode );
         });
-      }, $scope.switchdelay );
+      }, wait );
     } else {
       console.log( 'switch already in progress, please wait..' );
     }
@@ -131,9 +125,10 @@ function guiCtrl( $scope, $rootScope, $state, $stateParams, $window, tputFactory
     }
   };
   $scope.$on( '$destroy', function( event ) {
-    // be polite : cancel timeout(s)
+    // be polite : cancel $timeout(s)?
     $timeout.cancel( tputTimer );
-    // and then?
-    console.log( 'ok thank you' );
+    if ( QMIMO_FAKE_DEMO === true ) {
+      $timeout.cancel( tputGenTimer );
+    }
   });
 }
